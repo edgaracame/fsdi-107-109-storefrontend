e4b61ed40c4c1422e50dbb4e9ff88b9e957cfac8
edgarcastillo17 <60405064+edgaracame@users.noreply.github.com>
2022-06-21 19:32:02 -0700

    Class 1: Assignment 1
    Please deliver a Simple React project with:
    
    - NavBar component
    - Footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+function Footer(){
+    return(
+        <div className="footer">
+            <p>&copy; Castillo Medina Edgar Antonio 2022</p>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import "./navbar.css";
+
+function Navbar(){
+    return(
+        <div className="navbar">
+            <h1>Aerkadia</h1>
+        </div>
+    );
+}
+
+export default Navbar;
